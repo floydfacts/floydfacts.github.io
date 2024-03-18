c97a5fc7b07b495d8fedc4ec79a2c4615acbffa2
floydfacts <163212814+floydfacts@users.noreply.github.com>
2024-03-18 05:14:28 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -43,13 +43,13 @@ function includeHTML() {
         })
   }; 
 
-/* Dark Mode ######################################### */
+/* Dark Mode ######################################### 
 
 function dark() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
-
+*/
 
 
 
